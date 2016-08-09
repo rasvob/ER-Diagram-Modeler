fddfeaee8d79fc4fb9115e4969663f306d1e6b0a
Radek Svoboda <rasvob14@gmail.com>
2016-08-09 16:34:17 +0200

    Resize re-invented

--- ER Diagram Modeler/Views/Canvas/TableItem/ResizeThumb.cs
+++ ER Diagram Modeler/Views/Canvas/TableItem/ResizeThumb.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -42,44 +43,54 @@ namespace ER_Diagram_Modeler.Views.Canvas.TableItem
 				double maxTop = double.MinValue;
 				double maxLeft = double.MinValue;
 
-				double deltaVertical, deltaHorizontal;
 				double minDeltaVertical = double.MaxValue; 
 				double minDeltaHorizontal = double.MaxValue;
+
+				double maxDeltaVertical = double.MaxValue;
+				double maxDeltaHorizontal = double.MaxValue;
 
 				foreach(TableContent item in _canvas.SelectedItems)
 				{
 					minLeft = Math.Min(DesignerCanvas.GetLeft(item), minLeft);
 					minTop = Math.Min(DesignerCanvas.GetTop(item), minTop);
 
+					maxLeft = Math.Max(DesignerCanvas.GetLeft(item) + item.ActualWidth, maxLeft);
+					maxTop = Math.Max(DesignerCanvas.GetTop(item) + item.ActualHeight, maxTop);
+
 					minDeltaVertical = Math.Min(item.ActualHeight - item.MinHeight, minDeltaVertical);
 					minDeltaHorizontal = Math.Min(item.ActualWidth - item.MinWidth, minDeltaHorizontal);
+
+					maxDeltaVertical = Math.Min(_canvas.ActualHeight - (DesignerCanvas.GetTop(item) + item.ActualHeight), maxDeltaVertical);
+					maxDeltaHorizontal = Math.Min(_canvas.ActualWidth - (DesignerCanvas.GetLeft(item) + item.ActualWidth), maxDeltaHorizontal);
 				}
 
 				foreach(TableContent item in _canvas.SelectedItems)
 				{
+					double deltaVertical;
 					switch (VerticalAlignment)
 					{
 						case VerticalAlignment.Bottom:
-							deltaVertical = Math.Min(-e.VerticalChange, minDeltaVertical);
+							deltaVertical = e.VerticalChange > 0 && e.VerticalChange >= maxDeltaVertical ? 0 : Math.Min(-e.VerticalChange, minDeltaVertical);
 							item.Height = item.ActualHeight - deltaVertical;
 							break;
 						case VerticalAlignment.Top:
-							deltaVertical = Math.Min(e.VerticalChange, _item.ActualHeight - _item.MinHeight);
-							System.Windows.Controls.Canvas.SetTop(_item, System.Windows.Controls.Canvas.GetTop(_item) + deltaVertical);
-							_item.Height -= deltaVertical;
+							deltaVertical = Math.Min(Math.Max(-minTop, e.VerticalChange), minDeltaVertical);
+							DesignerCanvas.SetTop(item, DesignerCanvas.GetTop(item) + deltaVertical);
+							item.Height = item.ActualHeight - deltaVertical;
 							break;
 					}
 
+					double deltaHorizontal;
 					switch (HorizontalAlignment)
 					{
 						case HorizontalAlignment.Left:
-							deltaHorizontal = Math.Min(e.HorizontalChange, _item.ActualWidth - _item.MinWidth);
-							System.Windows.Controls.Canvas.SetLeft(_item, System.Windows.Controls.Canvas.GetLeft(_item) + deltaHorizontal);
-							_item.Width -= deltaHorizontal;
+							deltaHorizontal = Math.Min(Math.Max(-minLeft, e.HorizontalChange), minDeltaHorizontal);
+							DesignerCanvas.SetLeft(item, DesignerCanvas.GetLeft(item) + deltaHorizontal);
+							item.Width = item.ActualWidth - deltaHorizontal;
 							break;
 						case HorizontalAlignment.Right:
-							deltaHorizontal = Math.Min(-e.HorizontalChange, _item.ActualWidth - _item.MinWidth);
-							_item.Width -= deltaHorizontal;
+							deltaHorizontal = e.HorizontalChange > 0 && e.HorizontalChange >= maxDeltaHorizontal ? 0 : Math.Min(-e.HorizontalChange, minDeltaHorizontal);
+							item.Width = item.ActualWidth - deltaHorizontal;
 							break;
 					}
 				}
